e8d9b25fe257430367b49a0a4ffc91cdaba616be
jwatmore <jason.watmore@hotmail.com>
2020-07-02 10:58:07 +1000

    Updated to express-jwt version 6.0.0 to fix security vulnerability

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Node.js Role Based Authorization API
 
-For documentation and instructions check out http://jasonwatmore.com/post/2018/11/28/nodejs-role-based-authorization-tutorial-with-example-api
+For documentation and instructions check out https://jasonwatmore.com/post/2018/11/28/nodejs-role-based-authorization-tutorial-with-example-api
--- _helpers/authorize.js
+++ _helpers/authorize.js
@@ -1,4 +1,4 @@
-const expressJwt = require('express-jwt');
+const jwt = require('express-jwt');
 const { secret } = require('config.json');
 
 module.exports = authorize;
@@ -12,7 +12,7 @@ function authorize(roles = []) {
 
     return [
         // authenticate JWT token and attach user to request object (req.user)
-        expressJwt({ secret }),
+        jwt({ secret, algorithms: ['HS256'] }),
 
         // authorize based on user role
         (req, res, next) => {
